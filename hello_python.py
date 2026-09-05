e66b69a7fd99514eaa79b3728ec30e7c7bc17a6f
anakhjot <charanjitkaur19q@gmail.com>
2026-09-05 21:55:32 +0530

    update python script

--- hello_python.py
+++ hello_python.py
@@ -1,3 +1,17 @@
 name = input("What is your name? ")
 print("Hello,", name)
 print("Welcome to your Python learning journey!")
+
+#variable
+
+name = 'anakhjot'    #string
+age = 18             #integar
+hight = 5.8          #float
+is_student = True    #boolean
+
+print(name)
+print(age)
+
+age = age+1
+print(age)          #19
+
